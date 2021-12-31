8680ef628e202e7969fb7648307e7b15e5e46796
wanghao <1750110604@qq.com>
2021-12-31 15:50:21 +0800

    更新

--- pages/_app.js
+++ pages/_app.js
@@ -4,23 +4,23 @@ import { Popover, Transition } from '@headlessui/react'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Popover className="relative bg-white">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6">
-        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-          <Popover.Group as="nav" className="hidden md:flex space-x-10">
-            <a href="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
-              home
-            </a>
-            <a href="/test" className="text-base font-medium text-gray-500 hover:text-gray-900">
-              test
-            </a>
-          </Popover.Group>
+      <>
+        <div className="px-80 mx-auto px-4 sm:px-6">
+            <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+            <Popover.Group as="nav" className="hidden md:flex space-x-10">
+                <a href="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                home
+                </a>
+                <a href="/test" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                test
+                </a>
+            </Popover.Group>
+            </div>
         </div>
-      </div>
-      <div className='mx-auto max-w-7xl sm:px-6'>
-        <Component {...pageProps} />
-      </div>
-    </Popover>
+        <div className='px-80 mx-auto sm:px-6 mt-8'>
+            <Component {...pageProps} />
+        </div>
+      </>
   )
 }
 
