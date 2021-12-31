829675772399e3db7b9650b67abb00fa0bf02772
wanghao <1750110604@qq.com>
2021-12-31 15:34:03 +0800

    引入tailwindcss

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+import { Popover, Transition } from '@headlessui/react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Popover className="relative bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+          <Popover.Group as="nav" className="hidden md:flex space-x-10">
+            <a href="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              home
+            </a>
+            <a href="/test" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              test
+            </a>
+          </Popover.Group>
+        </div>
+      </div>
+      <div className='mx-auto max-w-7xl sm:px-6'>
+        <Component {...pageProps} />
+      </div>
+    </Popover>
+  )
 }
 
 export default MyApp
--- pages/test.js
+++ pages/test.js
@@ -14,6 +14,32 @@ export default function Test() {
             height: '30px',
             color: 'red'
         }} />
+        <table class="table-auto border text-center">
+            <thead>
+                <tr>
+                    <th class="border w-1/2">Title</th>
+                    <th class="border">Author</th>
+                    <th class="border">Views</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="border">Intro to CSS</td>
+                    <td class="border">Adam</td>
+                    <td class="border">858</td>
+                </tr>
+                <tr>
+                    <td class="border">A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
+                    <td class="border">Adam</td>
+                    <td class="border">112</td>
+                </tr>
+                <tr>
+                    <td class="border">Intro to JavaScript</td>
+                    <td class="border">Chris</td>
+                    <td class="border">1,280</td>
+                </tr>
+            </tbody>
+            </table>
     </div>
   )
 }
