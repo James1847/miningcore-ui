fe1e34dd35f32b6b25962d377af9a1c62781fef3
wanghao <1750110604@qq.com>
2021-12-31 14:41:30 +0800

    update

---
+++ pages/test.js
@@ -0,0 +1,19 @@
+import { BeakerIcon, ChatIcon } from '@heroicons/react/outline'
+
+
+export default function Test() {
+  return (
+    <div>
+        <BeakerIcon style={{
+            width: '30px',
+            height: '30px',
+            color: 'red'
+        }} />
+        <ChatIcon style={{
+            width: '30px',
+            height: '30px',
+            color: 'red'
+        }} />
+    </div>
+  )
+}
