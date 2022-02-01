c43d20160c19a42eac5aa3c8c72f3d87ee0f02fc
James <1536755789@qq.com>
2022-02-01 22:22:46 +0800

    chagne BaseURL to https://mafrapool.com/api/pools

--- request/request.js
+++ request/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const BASE_URL = 'https://miningcoreui.cxdnb.top/api/pools'// 生产
+const BASE_URL = 'https://mafrapool.com/api/pools'// 生产
 // const BASE_URL = 'http://13.212.95.157:4000/api/pools'// 测试
 const service = axios.create({
   baseURL: BASE_URL, // url = base url + request url
